8258f33131111575496b0506d641e91d5e0b21d8
unknown <bdaseh@gmail.com>
2021-08-20 18:07:42 -0700

    colored chart bars

--- script.js
+++ script.js
@@ -10,14 +10,9 @@ async function graphData() {
             datasets: [{
                 label: 'Global Average Temp in C',
                 data: gData.yData,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
+                backgroundColor: 
+                    'rgba(255, 0, 64, 0.8)',
+                    
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
